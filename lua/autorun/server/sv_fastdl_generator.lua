e1c71be03163f65aab268a006826f24dd3a7bdd9
joeyjumper94 <joeyjumper94@gmail.com>
2018-03-26 22:34:46 -0700

    forgot a word in the instruction

--- lua/autorun/server/sv_fastdl_generator.lua
+++ lua/autorun/server/sv_fastdl_generator.lua
@@ -9,10 +9,10 @@ concommand.Add("generate_fastdl",function(ply,cmd,args)
 	end
 	file.Write('revenants_fastdl.txt',str)
 	if ply and ply:IsValid() and !ply:IsListerServerHost() then
-		ply:PrintMessage(HUD_PRINTTALK,'check the server\'s data folder, there should be a file named "revenants_fastdl.txt"')
-		ply:PrintMessage(HUD_PRINTTALK,'Rename it to revenants_fastdl.lua and put it in the "garrysmod/lua/autorun/server"')
+		ply:PrintMessage(HUD_PRINTCONSOLE,'check the server\'s data folder, there should be a file named "revenants_fastdl.txt"')
+		ply:PrintMessage(HUD_PRINTCONSOLE,'Rename it to revenants_fastdl.lua and put it in the "garrysmod/lua/autorun/server"')
 	else
-		print'check server\'s data folder, there should be a file named "revenants_fastdl.txt"'
+		print'check the server\'s data folder, there should be a file named "revenants_fastdl.txt"'
 		print'Rename it to revenants_fastdl.lua and put it in the "garrysmod/lua/autorun/server"'
 	end
 end)
