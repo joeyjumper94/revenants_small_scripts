c9d0ac6284c3f03fda5fde56507dd82629664544
joeyjumper94 <joeyjumper94@gmail.com>
2018-08-11 19:53:39 -0700

    added a script that fixes an exploit related to duplicator and LuaJIT

--- lua/autorun/sh_duplicator_spawn_restrictor.lua
+++ lua/autorun/sh_duplicator_spawn_restrictor.lua
@@ -1,7 +1,7 @@
 local RANK=CreateConVar("duplicator_restrict_spawns", "trusted", {FCVAR_ARCHIVE,FCVAR_SERVER_CAN_EXECUTE,FCVAR_REPLICATED},'decides what rank can spawn dupes, case sensitive, use "none", "nil", "false", "0", or "" for unrestricted'):GetString()
 hook.Add("CanTool","duplicator_restrictor",function(ply,trace,tool)
 	if ply and tool then
-		if tool=="duplicator" and !ply:CheckGroup(RANK) and !ply:IsSuperAdmin() and ply:KeyPressed(IN_ATTACK) and RANK!="nil" and RANK!="" and RANK!="none" and tobool(RANK) then
+		if tool=="duplicator" and !ply:CheckGroup(RANK) and !ply:IsSuperAdmin() and !ply:KeyPressed(IN_ATTACK2) and RANK!="nil" and RANK!="" and RANK!="none" and tobool(RANK) then
 		--the first check returns true if the player is using duplicator
 		--the second check returns true if the player's rank neither matches nor inherits from trusted. ULX is required
 		--the third check returns true if the player is not an admin
